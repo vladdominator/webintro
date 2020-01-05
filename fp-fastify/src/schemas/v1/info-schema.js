f1f8c23f56ba83c813b91dab899aa4a8190fe740
aperez <aperez@pangramia.com>
2020-01-05 23:36:49 +0300

    Intermediate yate templating

--- fp-fastify/src/schemas/v1/info-schema.js
+++ fp-fastify/src/schemas/v1/info-schema.js
@@ -22,5 +22,29 @@ module.exports = {
         }
       }
     }
+  },
+  tripsStatus: {
+    description: 'Get the status of the current trips',
+    tags: ['Trip'],
+    summary: 'Obtain the status of the current trips',
+    response: {
+      200: {
+        description: 'Succesful response',
+        type: 'string'
+      },
+      400: {
+        description: 'Not Found response',
+        type: 'string'
+      },
+      500: {
+        description: 'Error response',
+        type: 'object',
+        properties: {
+          error: { type: 'string' },
+          message: { type: 'string' },
+          statusCode: { type: 'number' }
+        }
+      }
+    }
   }
 }
