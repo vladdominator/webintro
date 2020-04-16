4f1f8b66e3ef935cca10b1d2baef3aa7492b5f67
aperez <aperez@pangramia.com>
2020-04-16 13:04:53 +0300

    Simplify API to order part only

--- fp-service-orders2/codegen/ngVersion/model/models.ts
+++ fp-service-orders2/codegen/ngVersion/model/models.ts
@@ -1,6 +1,4 @@
-export * from './city';
 export * from './errorResponse';
-export * from './pathEstimationResponse';
 export * from './teaOrder';
 export * from './teaSpot';
 export * from './teaType';
