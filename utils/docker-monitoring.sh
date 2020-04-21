7a55205c4963153fc0b7f1eddaf61adee45c6b9a
aperez <aperez@pangramia.com>
2020-04-21 22:52:17 +0300

    Add docker yandextank and monitoring

---
+++ utils/docker-monitoring.sh
@@ -0,0 +1,10 @@
+VERSION=v0.35.0 # use the latest release version from https://github.com/google/cadvisor/releases
+sudo docker run \
+  --volume=/:/rootfs:ro \
+  --volume=/var/run:/var/run:rw \
+  --volume=/sys:/sys:ro \
+  --volume=/var/lib/docker/:/var/lib/docker:ro \
+  --publish=8080:8080 \
+  --detach=true \
+  --name=cadvisor \
+  google/cadvisor:$VERSION
