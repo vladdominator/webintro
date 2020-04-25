ba32c331f0077e84724ef4a4bfe602b22a89af7b
aperez <aperez@pangramia.com>
2020-04-25 21:45:02 +0300

    Fix logger, health endpoint and packages for orders service

--- fp-service-orders/app.js
+++ fp-service-orders/app.js
@@ -3,6 +3,7 @@
 var SwaggerExpress = require('swagger-express-mw');
 const express = require('express');
 const path = require('path');
+const logger = require('./logger');
 var app = express();
 
 module.exports = app; // for testing
@@ -17,6 +18,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // to expose health check 
 app.use('/health', require('express-healthcheck')());
 
+// we don't integrate morgan or swagger-logger to winston here yet
 
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
@@ -24,15 +26,13 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   // install middleware
   swaggerExpress.register(app);
 
-
-
   var port = process.env.PORT || 10011;
   app.listen(port);
 
-  console.log('check this to see docs :\n  http://127.0.0.1:' + port + '/');
+  logger.info('check this to see docs :\n  http://127.0.0.1:' + port + '/');
   
   if (swaggerExpress.runner.swagger.paths['/tea/orders']) {
-  	console.log('try this to test:\ncurl http://127.0.0.1:' + port + '/api/v1/tea/orders');
+  	logger.info('try this to test:\ncurl http://127.0.0.1:' + port + '/api/v1/tea/orders');
 
   }
 });
--- fp-service-orders/ecosystem.config.js
+++ fp-service-orders/ecosystem.config.js
@@ -27,16 +27,5 @@ module.exports = {
       instances: 1,
       PORT: 10011
     }
-  }],
-
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+  }] 
 };
