337ec0954d5afbcd114b7c89c473342239d3b956
aperez <aperez@pangramia.com>
2020-04-25 15:32:14 +0300

    Add pm2 instance cluster depiction

--- fp-service-feedback/app.js
+++ fp-service-feedback/app.js
@@ -26,18 +26,12 @@ const hbs = exphbs.create({
 
 HandlebarsIntl.registerWith(hbs.handlebars)
 
-
-
-
 // view engine setup
 //exphbs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
 
 app.engine('handlebars', hbs.engine);
-
-
-
 
 
 // app.use(favicon(__dirname + '/public/img/favicon.ico'));
@@ -50,8 +44,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/feedback', routes);
 app.use('/feedback/reviews', feedback);
 app.use('/feedback/dates', dates);
+app.use('/health', require('express-healthcheck')());
+
 
 /// catch 404 and forward to error handler
 app.use((req, res, next) => {
--- fp-service-feedback/routes/index.js
+++ fp-service-feedback/routes/index.js
@@ -4,14 +4,20 @@ const router = express.Router();
 const util = require('util')
 const chance = require('chance')
 const faker = require('faker')
- 
+
+const packageGenVersion = require('../lib/version.js') 
+const pm_id = process.env.pm_id || 0
+const instance_id = process.env.NODE_APP_INSTANCE || 0 
+
 
 /* GET home page. */
 router.get('/', (req, res) => {
 	res.render('index', 
 	  	{ team:  'FP', 
 	  	  year: 2020,
-	  	  version: "0.2.3"
+	  	  version: packageGenVersion,
+	  	  id: pm_id,
+	  	  instance: instance_id
 	  	});
 
   
