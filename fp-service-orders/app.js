486c72c5fed35be7794726fa97f97185aaad24aa
aperez <aperez@pangramia.com>
2020-04-25 22:16:11 +0300

    Add swagger-stats

--- fp-service-orders/app.js
+++ fp-service-orders/app.js
@@ -1,9 +1,16 @@
 'use strict';
 
+var appRoot = require('app-root-path');
+var reqlib = appRoot.require;
+
 var SwaggerExpress = require('swagger-express-mw');
 const express = require('express');
+const swStats = require('swagger-stats');
+const apiSpec = reqlib('api/swagger/openapi.json')
+
 const path = require('path');
-const logger = require('./logger');
+const logger = reqlib('logger');
+
 var app = express();
 
 module.exports = app; // for testing
@@ -23,8 +30,12 @@ app.use('/health', require('express-healthcheck')());
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  // allow swagger-stats microservice monitoring by http://localhost:10011/swagger-stats/ux#/
+  app.use(swStats.getMiddleware({swaggerSpec:apiSpec}));
+
   // install middleware
   swaggerExpress.register(app);
+
 
   var port = process.env.PORT || 10011;
   app.listen(port);
